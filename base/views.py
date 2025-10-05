c6b7ae383de15a1bcb2bd3c68851a2b90d9d326e
KapishPathrikar <kapishpathrikar@gmail.com>
2025-10-05 16:03:47 +0530

    Django project Searchbar and login,logout

--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib import messages
+from django.db.models import Q          #Used for serach related operations.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from .models import Room,Topic
 from .forms import RoomForm     #importing after making Roomform in forms.py
 # Create your views here.
 
@@ -9,9 +13,41 @@ from .forms import RoomForm     #importing after making Roomform in forms.py
 #     {'id': 3, 'name':'Frontend dev'},
 # ]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+        user = authenticate(request, username=username,password = password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or Password does not exist")
+    context = {}
+    return render(request,'base/login_register.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''      #if request has q variable then it filters but if none exist with such name then all are displayed. All button doenst have q therefore everything is displayed 
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q)|
+        Q(description__icontains=q)
+        )       #contains sees for eg if for searching python only py is added to sitename then it searches for rooms starting with py.(full python isnt needed to be typed) and icontains = case insensitive (contains)
+                # by using Q we put OR for searching either room name or room topic or room description
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms':rooms,'topics':topics, 'room_count':room_count}
     return render(request, 'base/home.html', context)
 
 def room(request,pk):
