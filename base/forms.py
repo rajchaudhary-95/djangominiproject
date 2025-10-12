c0a9d0c19cac145cec2cd564075fe41c1e8151ce
KapishPathrikar <kapishpathrikar@gmail.com>
2025-10-13 00:24:17 +0530

    Django project static files commit

--- base/forms.py
+++ base/forms.py
@@ -6,3 +6,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'      # Gives all the fields from models.py class Room 
+        exclude = ['host','participants']
