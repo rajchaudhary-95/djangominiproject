66e1c4a8c20a65c7a693cfedbfb478661fe18105
rajchaudhary-95 <rajschaudhary06work@gmail.com>
2025-10-13 23:59:08 +0530

    Django project Theme 1.3

--- base/views.py
+++ base/views.py
@@ -103,32 +103,40 @@ def userProfile(request, pk):
 @login_required(login_url='/login')
 def createRoom(request):
     form = RoomForm()
-
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)      # passing in all the POST data in the form
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)   
+        
+        
+        Room.objects.create(
+            host = request.user,
+            topic = topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description'),
+        )
+        return redirect('home')
+    context = {'form': form,'topics':topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='/login')
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)     # Form will be prefilled with room value to edit that specific room
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse("You are not allowed here!")
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)        #Specifying which room to update
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
 
-    context={'form': form}
+    context={'form': form,'topics':topics, 'room':room}
     return render(request, 'base/room_form.html',context)
 
 @login_required(login_url='/login')
