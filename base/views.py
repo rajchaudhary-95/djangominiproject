6a0c7aababb1ba08dfe4cef46d374ed301f292a9
KapishPathrikar <kapishpathrikar@gmail.com>
2025-10-14 22:58:50 +0530

    Django project complete user model custom commit

--- base/views.py
+++ base/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q, Count          #Used for serach related operations.
-from django.contrib.auth.models import User
+
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room,Topic,Message
-from .forms import RoomForm, UserForm     #importing after making Roomform in forms.py
+from .models import Room,Topic,Message,User
+from .forms import RoomForm, UserForm, MyUserCreationForm     #importing after making Roomform in forms.py
 # Create your views here.
 
 # rooms = [
@@ -22,14 +22,14 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, "User does not exist")
-        user = authenticate(request, username=username,password = password)
+        user = authenticate(request, email=email,password = password)
         if user is not None:
             login(request,user)
             return redirect('home')
@@ -44,10 +44,10 @@ def logoutUser(request):
     return redirect('home')
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)          #when form is valid the user is gonna get created so commit false makes access the user right away
             user.username = user.username.lower()
@@ -173,7 +173,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST,request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile',pk=user.id)
