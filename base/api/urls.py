b7b1ce808299dfecadf1df7e5f141657e86c80ca
KapishPathrikar <kapishpathrikar@gmail.com>
2025-10-14 22:00:15 +0530

    Django project custom user model 1.1 commit

--- base/api/urls.py
+++ base/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes),
-    path('room/', views.getRooms)
 
+    path('', views.getRoutes),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>/', views.getRoom),
 ]
